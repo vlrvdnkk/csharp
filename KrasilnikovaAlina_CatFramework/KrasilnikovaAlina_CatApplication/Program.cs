962396e6e14b007a91c60e1bc5a3ec2e9e113125
Алина Красильникова <KrasilnikovaAA21@st.ithub.ru>
2023-11-16 15:26:42 +0300

    кот/д (крутой)

---
+++ KrasilnikovaAlina_CatFramework/KrasilnikovaAlina_CatApplication/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using KrasilnikovaAlina_CatFramework;
+
+class Program
+{
+    static Random random = new Random();
+
+    static Cat[] GenerateRandomCats(uint count)
+    {
+        Cat[] cats = new Cat[count];
+        for (int i = 0; i < count; i++)
+        {
+            try
+            {
+                if (random.Next(2) == 0)
+                {
+                    double weight = random.Next(50, 161);
+                    int fluffiness = random.Next(-20, 121);
+                    cats[i] = new Tiger(weight, fluffiness);
+                }
+                else
+                {
+                    int fluffiness = random.Next(-20, 121);
+                    cats[i] = new CuteCat(fluffiness);
+                }
+            }
+            catch (CatException ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                i--;
+            }
+        }
+        return cats;
+    }
+
+    static void DisplayCatInfo(Cat[] catsArr, string path)
+    {
+        using (StreamWriter writer = File.CreateText(path))
+        {
+            foreach (var cat in catsArr)
+            {
+                Console.WriteLine($"Fluffiness Check: {cat.FluffinessCheck()}");
+                Console.WriteLine(cat);
+                Console.WriteLine();
+
+                writer.WriteLine($"Fluffiness Check: {cat.FluffinessCheck()}");
+                writer.WriteLine(cat);
+                writer.WriteLine();
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of cats to generate: ");
+        uint catCount;
+        if (uint.TryParse(Console.ReadLine(), out catCount))
+        {
+            Cat[] cats = GenerateRandomCats(catCount);
+
+            Console.Write("Enter the path to save cat info: ");
+            string path = Console.ReadLine();
+
+            DisplayCatInfo(cats, path);
+            Console.WriteLine("Cat info has been saved.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
---
+++ KrasilnikovaAlina_CatFramework/KrasilnikovaAlina_CatFramework/Class1.cs
@@ -0,0 +1,99 @@
+﻿namespace KrasilnikovaAlina_CatFramework;
+
+using System;
+
+public class CatException : ArgumentException
+{
+    public CatException(string message) : base(message)
+    {
+    }
+}
+
+public abstract class Cat
+{
+    public abstract int Fluffiness { get; }
+
+    public abstract string FluffinessCheck();
+
+    public override string ToString()
+    {
+        return $"A cat with fluffiness: {Fluffiness}";
+    }
+}
+
+public class Tiger : Cat
+{
+    private double weight;
+    private int fluffiness;
+
+    public override int Fluffiness
+    {
+        get { return fluffiness; }
+    }
+
+    public double Weight
+    {
+        get { return weight; }
+    }
+
+    public Tiger(double weight = 50, int fluffiness = 50)
+    {
+        if ((weight < 75.0 || weight > 140.0) & (fluffiness < 0 || fluffiness > 100))
+            throw new CatException($"Unable to create a tiger with weight: {weight} and fluffiness: {fluffiness}");
+
+        if (fluffiness < 0 || fluffiness > 100)
+            throw new CatException($"Unable to create a tiger with fluffiness: {fluffiness}");
+        
+        if (weight < 75.0 || weight > 140.0)
+            throw new CatException($"Unable to create a tiger with weight: {weight}");
+
+        this.weight = weight;
+    }
+
+    public override string FluffinessCheck()
+    {
+        return "Kycь!";
+    }
+
+    public override string ToString()
+    {
+        return $"A tiger with weight: {Weight} fluffiness: {Fluffiness}";
+    }
+}
+
+public class CuteCat : Cat
+{
+    private int fluffiness;
+
+    public override int Fluffiness
+    {
+        get { return fluffiness; }
+    }
+
+    public CuteCat(int fluffiness = 50)
+    {
+        if (fluffiness < 0 || fluffiness > 140)
+            throw new CatException($"Unable to create a cute cat with fluffiness: {fluffiness}");
+
+        this.fluffiness = fluffiness;
+    }
+
+    public override string FluffinessCheck()
+    {
+        if (fluffiness == 0)
+            return "Sphynx";
+        else if (fluffiness <= 20)
+            return "Slightly";
+        else if (fluffiness <= 50)
+            return "Medium";
+        else if (fluffiness <= 75)
+            return "Heavy";
+        else
+            return "OwO";
+    }
+
+    public override string ToString()
+    {
+        return $"A cute cat with fluffiness: {Fluffiness}";
+    }
+}
